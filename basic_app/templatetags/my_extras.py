159473ebf71b9d3b75ed9f992a8ba7162fce312f
ChristinaGaitan <christina.gaitan@gmail.com>
2021-01-26 09:13:27 -0600

    Use decorator for creating custom filters

--- basic_app/templatetags/my_extras.py
+++ basic_app/templatetags/my_extras.py
@@ -1,11 +1,12 @@
 from django import template
 
-register = template.Library()
+# register = template.Library()
 
+@register.filter(name='cut') # this is a decorator
 def filter_cut(value, arg):
   """
     This cuts out all values of "arg" from the string
   """
   return value.replace(arg, '')
 
-register.filter('cut', filter_cut)
+# register.filter('cut', filter_cut)
